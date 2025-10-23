366e87eded85b9ea147b76855bca6bf00a31445c
MuhRizkiRamadhan24 <60300872@udst.edu.qa>
2025-10-23 22:23:58 +0300

    fixed the rrors in handlebars

--- infs3201_ap_f2025-2/business.js
+++ infs3201_ap_f2025-2/business.js
@@ -1,85 +1,60 @@
-const persistence = require('./persistence')
+const persistence = require('./persistence');
 
 /**
- * Get a photo by ID and populate album names
- * @param {string} photoId
- * @returns {Promise<Object|null>}
+ * Get photo details along with album names.
  */
 async function getPhotoDetails(photoId) {
-    const photo = await persistence.getPhotoDetails(photoId)
-    if (!photo) return null
-
-    const albumNames = []
-    for (let aid of photo.albums) {
-        const album = await persistence.getAlbumDetails(aid)
-        if (album) albumNames.push(album.name)
+    const photoDetails = await persistence.getPhotoDetails(photoId);
+    if (photoDetails) {
+        const albumNames = [];
+        for (let aid of photoDetails.albums) {
+            const album = await persistence.getAlbumDetails(aid);
+            if (album) albumNames.push(album.name);
+        }
+        photoDetails.albumNames = albumNames;
     }
-    photo.albumNames = albumNames
-    return photo
+    return photoDetails;
 }
 
 /**
- * Update photo title and description
- * @param {string} photoId
- * @param {string} title
- * @param {string} description
- * @returns {Promise<boolean>}
+ * Update photo title and description.
  */
 async function updatePhoto(photoId, title, description) {
-    return await persistence.updatePhoto(photoId, title, description)
+    return await persistence.updatePhoto(photoId, title, description);
 }
 
 /**
- * Get all albums
- * @returns {Promise<Array>}
- */
-async function getAllAlbums() {
-    return await persistence.getAllAlbums()
-}
-
-/**
- * Get album by ID
- * @param {string} albumId
- * @returns {Promise<Object|null>}
- */
-async function getAlbumDetails(albumId) {
-    return await persistence.getAlbumDetails(albumId)
-}
-
-/**
- * Get album by name
- * @param {string} name
- * @returns {Promise<Object|null>}
+ * Get album by name.
  */
 async function getAlbumDetailsByName(name) {
-    return await persistence.getAlbumDetailsByName(name)
+    return await persistence.getAlbumByName(name);
 }
 
+
 /**
- * Get all photos in an album
- * @param {string} albumId
- * @returns {Promise<Array>}
+ * Get all photos in an album.
  */
 async function getPhotosInAlbum(albumId) {
-    return await persistence.getPhotosInAlbum(albumId)
+    return await persistence.getPhotosInAlbum(albumId);
 }
 
 /**
- * Add tag to a photo
- * @param {string} photoId
- * @param {string} tag
- * @returns {Promise<boolean>}
+ * Add tag to a photo.
  */
 async function addTag(photoId, tag) {
-    return await persistence.addTag(photoId, tag)
+    return await persistence.addTag(photoId, tag);
+}
+
+async function getAllAlbums() {
+    const databaseAlbums = await persistence.getAllAlbums();
+    return databaseAlbums;
 }
 
 module.exports = {
     getPhotoDetails,
     updatePhoto,
-    getAllAlbums,
-    getAlbumDetails,
     getAlbumDetailsByName,
     getPhotosInAlbum,
     addTag
-}
+};
+
